1f1a2d3fd0bbce8e709b2e941f61d218d6c67fc0
Growthack90 <hackmaker90@outlook.it>
2021-01-07 17:42:19 +0100

    funzionamento bottone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      quote: ""
+    }
+  }
+
+  getQuote = () => {
+    console.log("It works")
+    this.setState({
+      quote: "It works"
+    })
+  }
+
+  render() {
+    const {quote} = this.state
+    return (
+      <div className="App">
+        <header className="App-header">
+          <button onClick= {() => this.getQuote()}>Start!</button>
+          <div>{quote}</div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
